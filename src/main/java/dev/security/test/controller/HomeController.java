06e3757f1b160bedfc5e712f5187a1485dfefd46
Johns David <rakotonindrianajohns@gmail.com>
2024-10-09 16:22:27 +0300

    first commit

---
+++ src/main/java/dev/security/test/controller/HomeController.java
@@ -0,0 +1,17 @@
+package dev.security.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("")
+    public  String Home(){
+        return "Hello, Home!";
+    }
+
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello, Secured!";
+    }
+}
---
+++ src/test/java/dev/security/test/SpendingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.security.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
